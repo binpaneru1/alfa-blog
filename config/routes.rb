cde2512d41fa61aa67e0a857b2f160bf25531839
binodpaneru <binod@fuelpanda.com>
2021-04-19 09:39:06 +0545

    add user create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, expect: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
